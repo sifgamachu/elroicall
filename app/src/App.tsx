ab54d0ea9f0ecec498b2cda1e853a75e77b415c9
sifgamachu <31213406+sifgamachu@users.noreply.github.com>
2026-09-07 00:45:48 -0400

    Make React own intake gift redemption and about routes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,16 @@
 import { useEffect } from "react";
-import { Routes, Route } from "react-router";
+import { Route, Routes } from "react-router";
+import About from "./pages/About";
+import Begin from "./pages/Begin";
+import Gift from "./pages/Gift";
+import GiftRedeem from "./pages/GiftRedeem";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
-/** Members currently live at /account/ while the portal is being unified. */
+/**
+ * The member portal is the last legacy standalone application while its
+ * authentication/data contract is moved into the shared React app.
+ */
 function ToAccount() {
   useEffect(() => {
     window.location.replace("/account/");
@@ -15,6 +22,16 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/begin" element={<Begin />} />
+      <Route path="/begin/" element={<Begin />} />
+      <Route path="/gift" element={<Gift />} />
+      <Route path="/gift/" element={<Gift />} />
+      <Route path="/g" element={<GiftRedeem />} />
+      <Route path="/g/" element={<GiftRedeem />} />
+      <Route path="/g/:code" element={<GiftRedeem />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/about/" element={<About />} />
+      <Route path="/account" element={<ToAccount />} />
       <Route path="/login" element={<ToAccount />} />
       <Route path="/portal" element={<ToAccount />} />
       <Route path="*" element={<NotFound />} />
